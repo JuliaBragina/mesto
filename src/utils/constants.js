249dff2c183a540a26740a209b25e8bce9d071e6
Julia Bragina <triulya@yandex.ru>
2022-05-15 13:14:20 +0300

    Добавлен файл constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+//Для формы редактирования 
+export const popupEdit = document.querySelector('.popup-edit');
+export const popupEditForm = popupEdit.querySelector('.popup-edit__form');
+export const profileEditOpenButton = document.querySelector('.profile__edit-button');
+
+//Для формы добавления карточки 
+export const popupAddForm = document.querySelector('.popup-add__form');
+export const profileAddOpenButton = document.querySelector('.profile__add-button');
+
+export const popupUpdateFrom = document.querySelector('.popup-update');
+export const profileUpdateAvatarButtnon = document.querySelector('.profile__avatar');
+
+export const popupName = document.querySelector('.popup-edit__item_el_name');
+export const popupDescr = document.querySelector('.popup-edit__item_el_description');
